7a093ceb2a1de6210306d5df62f06c4b69f890be
lxxjxxki <63500833+lxxjxxki@users.noreply.github.com>
2026-04-16 04:45:38 +0900

    feat(rawr): scaffold Spring Boot project

---
+++ rawr-backend/src/main/java/com/rawr/RawrApplication.java
@@ -0,0 +1,11 @@
+package com.rawr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RawrApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RawrApplication.class, args);
+    }
+}
